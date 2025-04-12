8d560c3b6038db5280f744e467299ad7fa6f1c23
Mati Joseph <crucialniccur@gmail.com>
2025-04-12 21:20:19 +0300

    practice state

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [status, updateStatus] = useState(false);
+
+  function handleClick() {
+    // updateStatus((status) => {
+    // return status ? "ONN" : "OFF";
+    // });
+    updateStatus((status) => !status);
+  }
+
+  const color = status ? "red" : "white";
+
+  return (
+    <button onClick={handleClick} style={{ background: color }}>
+      {status ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
